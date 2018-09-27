cad3734607541187aee5733edd5391fa048da852
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2018-09-27 16:25:55 -0700

    homework3 initial commit

---
+++ Homework3/src/Homework3.java
@@ -0,0 +1,14 @@
+
+public class Homework3 {
+
+	public static void main(String[] args) {
+		int n;
+		n = 6 + 10 * 100;
+		System.out.println("26. " + n);
+		
+		n = (6 + 10) * 100;
+		System.out.println(n);
+
+	}
+
+}
