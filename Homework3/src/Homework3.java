59e1d35da3961576c3213bdf9ddfbed1c10d97ba
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2018-09-27 16:33:14 -0700

    finished problems

--- Homework3/src/Homework3.java
+++ Homework3/src/Homework3.java
@@ -2,13 +2,58 @@
 public class Homework3 {
 
 	public static void main(String[] args) {
+		//26
 		int n;
 		n = 6 + 10 * 100;
 		System.out.println("26. " + n);
 		
 		n = (6 + 10) * 100;
 		System.out.println(n);
+		System.out.println();
+		
+		//27
+	     n = 20;
+	     
+	     n++;
+	     System.out.println("27." + n);
 
+	     n = n + 2;
+	     System.out.println(n);
+
+	     n = n - 3;
+	     System.out.println(n);
+	     System.out.println();
+	     
+	     //28
+	     System.out.print("28.");
+	     for (int i=1; i<=6; i++)
+	     {
+	        System.out.println("Hello");
+	     }
+			System.out.println();
+			
+	     //29
+	     System.out.print("29.");
+	     for (int i=1; i<=9; i+=2)
+	     {
+	        System.out.println(i);
+	     }
+			System.out.println();
+			
+	     //30
+	     System.out.print("30.");
+	     for (int i=0; i<=8; i+=2)
+	     {
+	        System.out.println(i+1);
+	     }
+			System.out.println();
+			
+		//31
+			System.out.println("31.");
+		 for (int i=1; i<=5; i++)
+		 {
+		     System.out.println(2*i-1);
+		 }
 	}
 
 }
